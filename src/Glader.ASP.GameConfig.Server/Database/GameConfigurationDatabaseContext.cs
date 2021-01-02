9c3c2c0036cf23c6b428d4c794e116eca7450cc9
HelloKitty <andrew.blakely@ymail.com>
2021-01-02 01:23:40 -0600

    Added initial GameConfigurationDatabaseContext

---
+++ src/Glader.ASP.GameConfig.Server/Database/GameConfigurationDatabaseContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glader.ASP.GameConfig
+{
+	/// <summary>
+	/// <see cref="DbContext"/> implementation for Game Configuration tables.
+	/// </summary>
+	public sealed class GameConfigurationDatabaseContext : DbContext
+	{
+		//Tables with no suffic indicate it's global/account wide.
+		public DbSet<AccountKeybindConfiguration> AccountKeybindConfiguration { get; set; }
+
+		public GameConfigurationDatabaseContext(DbContextOptions<GameConfigurationDatabaseContext> options)
+			: base(options)
+		{
+
+		}
+
+		public GameConfigurationDatabaseContext()
+		{
+
+		}
+
+		/// <inheritdoc />
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
