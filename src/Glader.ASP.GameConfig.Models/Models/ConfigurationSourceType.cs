698ff6b664bbcee06a84e636c80962bf884805f3
HelloKitty <andrew.blakely@ymail.com>
2021-01-02 00:00:42 -0600

    Added ConfigurationSourceType

---
+++ src/Glader.ASP.GameConfig.Models/Models/ConfigurationSourceType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.GameConfig
+{
+	/// <summary>
+	/// Enumeration of source types for a configuration
+	/// data.
+	/// </summary>
+	public enum ConfigurationSourceType
+	{
+		/// <summary>
+		/// Default generated configuration data.
+		/// </summary>
+		Default = 0,
+
+		/// <summary>
+		/// Configuration data for the linked source of an account.
+		/// </summary>
+		Account = 1,
+
+		/// <summary>
+		/// Configuration data linked to a sub-account (character in games).
+		/// </summary>
+		Character = 2,
+	}
+}
