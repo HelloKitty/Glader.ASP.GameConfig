52771cb543a19ae2c9de23faa454ea1b23f53424
HelloKitty <andrew.blakely@ymail.com>
2021-01-02 01:14:06 -0600

    Added KeybindData model

---
+++ src/Glader.ASP.GameConfig.Server/Database/Tables/KeybindData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glader.ASP.GameConfig
+{
+	/// <summary>
+	/// Shared keybind data model.
+	/// </summary>
+	[Owned]
+	public sealed class KeybindData
+	{
+		/// <summary>
+		/// Binary blob representing the keybindings data.
+		/// </summary>
+		[Required]
+		public byte[] Data { get; set; }
+
+		public KeybindData(byte[] data)
+		{
+			Data = data ?? throw new ArgumentNullException(nameof(data));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public KeybindData()
+		{
+			
+		}
+	}
+}
