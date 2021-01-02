24f9c472fad85d7f3d527ad6fdb974becf66bc35
HelloKitty <andrew.blakely@ymail.com>
2021-01-01 23:52:39 -0600

    Added GameConfigQueryResponseCode

---
+++ src/Glader.ASP.GameConfig.Models/ResponseCodes/GameConfigQueryResponseCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.GameConfig
+{
+	/// <summary>
+	/// Response code valeus for game config queries.
+	/// </summary>
+	public enum GameConfigQueryResponseCode
+	{
+		Success = GladerEssentialsModelConstants.RESPONSE_CODE_SUCCESS_VALUE,
+
+		//TODO: Make this a Model Constant.
+		GeneralServerError = 2,
+
+		ContentNotFound = 3,
+	}
+}
