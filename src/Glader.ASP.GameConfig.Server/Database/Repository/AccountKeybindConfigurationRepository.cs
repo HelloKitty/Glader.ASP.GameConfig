3a1f2d2a2a024916e830e6737e88e7325302b765
HelloKitty <andrew.blakely@ymail.com>
2021-01-02 01:32:42 -0600

    Added IAccountKeybindConfigurationRepository

---
+++ src/Glader.ASP.GameConfig.Server/Database/Repository/AccountKeybindConfigurationRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glader.ASP.GameConfig
+{
+	/// <summary>
+	/// EF-Core Database Backed <see cref="IAccountKeybindConfigurationRepository"/>.
+	/// </summary>
+	public sealed class AccountKeybindConfigurationRepository : GeneralGenericCrudRepositoryProvider<int, AccountKeybindConfiguration>, IAccountKeybindConfigurationRepository
+	{
+		public AccountKeybindConfigurationRepository(GameConfigurationDatabaseContext context) 
+			: base(context.AccountKeybindConfiguration, context)
+		{
+
+		}
+	}
+}
---
+++ src/Glader.ASP.GameConfig.Server/Database/Repository/IAccountKeybindConfigurationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.GameConfig
+{
+	public interface IAccountKeybindConfigurationRepository : IGenericRepositoryCrudable<int, AccountKeybindConfiguration>
+	{
+
+	}
+}
